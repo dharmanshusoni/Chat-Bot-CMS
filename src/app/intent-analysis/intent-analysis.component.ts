b7a30ec70caba85928beb74e84eedde3833ffdff
Dharmanshu <dharmanshu4@gmail.com>
2023-01-16 23:05:35 +0530

    intent analysis set

--- src/app/intent-analysis/intent-analysis.component.ts
+++ src/app/intent-analysis/intent-analysis.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Chartist from 'chartist';
+declare var $: any;
 
 @Component({
   selector: 'app-intent-analysis',
@@ -10,6 +12,61 @@ export class IntentAnalysisComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.TotalUniqueBotVisits();
   }
 
+  TotalUniqueBotVisits() {
+    var datawebsiteViewsChart = {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      series: [
+        [5, 4, 3, 7, 5, 10, 3]
+      ]
+    };
+    var optionswebsiteViewsChart = {
+      horizontalBars: true,
+      axisY: {
+        showGrid: false,
+        offset:100
+      },
+      low: 0,
+      high: 10,
+      chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
+    };
+    var responsiveOptions: any[] = [
+      ['screen and (max-width: 640px)', {
+        seriesBarDistance: 5,
+        axisX: {
+          labelInterpolationFnc: function (value) {
+            return value[0];
+          }
+        }
+      }]
+    ];
+    var TotalUniqueBotVisits = new Chartist.Bar('#TotalUniqueBotVisits', datawebsiteViewsChart, optionswebsiteViewsChart,responsiveOptions);
+    this.startAnimationForBarChart(TotalUniqueBotVisits);
+  }
+
+  startAnimationForBarChart(chart) {
+    let seq2: any, delays2: any, durations2: any;
+
+    seq2 = 0;
+    delays2 = 80;
+    durations2 = 500;
+    chart.on('draw', function (data) {
+      if (data.type === 'bar') {
+        seq2++;
+        data.element.animate({
+          opacity: {
+            begin: seq2 * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: 'ease'
+          }
+        });
+      }
+    });
+
+    seq2 = 0;
+  };
 }
