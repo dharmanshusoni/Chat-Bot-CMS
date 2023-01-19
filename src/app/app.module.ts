7a0d854da0f2235e2b1d0ea83f86878b1a48b636
Dharmanshu <dharmanshu4@gmail.com>
2023-01-19 12:25:07 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,22 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 import { AgmCoreModule } from '@agm/core';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { HomeComponent } from './home/home.component';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { SessionTableComponent } from './session-table/session-table.component';
 import { SessionAnalysisComponent } from './session-analysis/session-analysis.component';
 import { IntentAnalysisComponent } from './intent-analysis/intent-analysis.component';
 import { FormsUserDataComponent } from './forms-user-data/forms-user-data.component';
 
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { TableListComponent } from './table-list/table-list.component';
+// import { TypographyComponent } from './typography/typography.component';
+// import { IconsComponent } from './icons/icons.component';
+// import { MapsComponent } from './maps/maps.component';
+// import { NotificationsComponent } from './notifications/notifications.component';
+// import { UpgradeComponent } from './upgrade/upgrade.component';
 
 @NgModule({
   imports: [
